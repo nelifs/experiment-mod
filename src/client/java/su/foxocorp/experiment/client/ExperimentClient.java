6b6bc9b468f400b67539f76759780dd0e0db9d4d
nelifs <nelifs@icloud.com>
2025-05-14 14:58:30 +0300

    Add client

---
+++ src/client/java/su/foxocorp/experiment/client/ExperimentClient.java
@@ -0,0 +1,13 @@
+package su.foxocorp.experiment.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ExperimentClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
