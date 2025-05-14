596cd90552d22dab5db299a07e32616180ec9c81
nelifs <nelifs@icloud.com>
2025-05-14 18:19:08 +0300

    Add modules call

--- src/main/java/su/foxocorp/experiment/Experiment.java
+++ src/main/java/su/foxocorp/experiment/Experiment.java
@@ -2,19 +2,24 @@ package su.foxocorp.experiment;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
-import su.foxocorp.experiment.modules.WorldBorder;
+import su.foxocorp.experiment.module.ActionBar;
+import su.foxocorp.experiment.module.WorldBorder;
 
 public class Experiment implements ModInitializer {
 
+    public static final String MOD_ID = "experiment";
+
+    private static final WorldBorder worldBorder = new WorldBorder();
+
+    private static final ActionBar actionBar = new ActionBar();
+
     @Override
     public void onInitialize() {
-        System.out.println("Experiment initialized");
+        System.out.println("Experiment Mod initialized");
 
         ServerTickEvents.END_SERVER_TICK.register(server -> {
-            WorldBorder worldBorder = new WorldBorder();
             worldBorder.tick(server);
+            actionBar.tick(server);
         });
     }
 }
